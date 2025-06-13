e00cb00e3f2a561a42057fbf6d85a6e5e815599f
satkriti <satkritikhadka2061@gmail.com>
2025-06-13 11:38:04 +0545

    new commit

---
+++ day 10/dataset.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df=pd.read("personality_dataset.csv")
+print(df.head(3))
---
+++ day11/dict.py
@@ -0,0 +1,17 @@
+# Define a dictionary containing employee data
+data1 = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+         'Age': [27, 24, 22, 32],
+         'Address': ['Kathmandu', 'Bhaktapur', 'Lalitpur', 'Pokhara']
+        }
+
+# Define a dictionary containing employee data
+data2 = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+         'Age': [27, 24, 22, 32],
+         'Grade': ['A', 'A', 'C', 'B']
+        }
+
+# Convert the dictionary into DataFrame
+df1 = pd.DataFrame(data1)
+
+# Convert the dictionary into DataFrame
+df2 = pd.DataFrame(data2)
